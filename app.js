92bea89e0df61e7ddd5c645c710faab14ae8bddf
Ishpaul Singh <ishpaul207@gmail.com>
2022-06-06 15:22:05 +0530

    made changes in styles

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const toggleMenu = document.querySelector('.toggle-menu');
 const closeBtn = document.querySelector('#close-btn');
 const menuItems = document.querySelectorAll(".menu-item")
 //for-top-nav
-function display2() {
+function display() {
     document.querySelector('.mobile-menu').style.display = "block";
     toggleMenu.style.display = "none";
 } 
 
-toggleMenu.addEventListener('click', display2)
+toggleMenu.addEventListener('click', display)
 
-function display(){
+function display2(){
     document.querySelector('.mobile-menu').style.display = "none";
     toggleMenu.style.display ="unset";
 } 
 
-closeBtn.addEventListener('click', display)
+closeBtn.addEventListener('click', display2)
 
 menuItems.forEach((item) => {
-    item.addEventListener('click', display)
+    item.addEventListener('click', display2)
 });
 
 
